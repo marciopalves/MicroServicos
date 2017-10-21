89147a63ab88e944eca7c72dd8be248c0d0364f6
pedrofsn <pedrokra@gmail.com>
2017-10-21 16:49:26 -0200

    Atualizando/cadastrando eventos apenas se o microserviço de usuários estiver disponível e se o id do usuário/criador for existente

--- node-microservice-eventos/api/evento/eventoService.js
+++ node-microservice-eventos/api/evento/eventoService.js
@@ -8,14 +8,7 @@ Evento.updateOptions({ new: true, runValidators: true })
 Evento.after('get', function (req, res, next) {
   var eventos = res.locals.bundle
 
-  request({
-    "method": "GET",
-    "uri": url_api_externa,
-    "json": true,
-    "headers": {
-      "User-Agent": "MicroserviceNodeJS"
-    }
-  }).then(function (body) {
+  consultaExterna(url_api_externa).then(function (body) {
     console.log("Dados oriundos do microserviço:" + body)
     res.json({ eventos, criadores: body })
   }
@@ -25,26 +18,31 @@ Evento.after('get', function (req, res, next) {
     });
 });
 
-Evento.before('post', function (req, res, next) {
+Evento.before('post', function (req, res, next) { hasUsuario(req, res, next); });
+Evento.before('put', function (req, res, next) { hasUsuario(req, res, next); });
+
+function hasUsuario(req, res, next) {
   var url = url_api_externa + req.body.idCriador
 
-  request({
-    "method": "GET",
-    "uri": url,
-    "json": true,
-    "headers": {
-      "User-Agent": "MicroserviceNodeJS"
-    }
-  }).then(function (body) {
-    //Só prossegue com o cadastro se o idCriador for equivalente a um usuário na API externa
-    next() 
-  }
-    , function (err) {
-      var erroAPIExterna = err.error
-      console.error("Erro da API externa:" + erroAPIExterna.mensagemDesenvolvedor);
-      res.status(erroAPIExterna.status).json({ erro: erroAPIExterna.titulo })
-    });
-});
+  //Só prossegue com a requisição/ação se o idCriador for equivalente a um usuário existente na API externa
+  consultaExterna(url).then(function (body) {
+    next();
+  }, function (err) {
+    var erroAPIExterna = err.error;
+    console.error("Erro da API externa:" + erroAPIExterna.mensagemDesenvolvedor);
+    res.status(erroAPIExterna.status).json({ erro: erroAPIExterna.titulo });
+  });
+}
 
+function consultaExterna(url) {
+  return request({
+    'method': 'GET',
+    'uri': url,
+    'json': true,
+    'headers': {
+      'User-Agent': 'MicroserviceNodeJS'
+    }
+  });
+}
 
 module.exports = Evento
