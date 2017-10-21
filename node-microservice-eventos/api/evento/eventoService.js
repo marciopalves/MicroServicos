f0dcdbb77954e16b9923e3ec5e4fd2d63cbc4cca
pedrofsn <pedrokra@gmail.com>
2017-10-21 16:05:40 -0200

    Só prossigo com o cadastro de evento se o microserviço de usuários estiver disponível e o usuário/idCriador for existente

--- node-microservice-eventos/api/evento/eventoService.js
+++ node-microservice-eventos/api/evento/eventoService.js
@@ -13,7 +13,7 @@ Evento.after('get', function (req, res, next) {
     "uri": url_api_externa,
     "json": true,
     "headers": {
-      "User-Agent": "My little demo app"
+      "User-Agent": "MicroserviceNodeJS"
     }
   }).then(function (body) {
     console.log("Dados oriundos do microserviço:" + body)
@@ -22,6 +22,27 @@ Evento.after('get', function (req, res, next) {
     , function (err) {
       console.error("Falha ao trazer dados do microserviço:" + err);
       next()
+    });
+});
+
+Evento.before('post', function (req, res, next) {
+  var url = url_api_externa + req.body.idCriador
+
+  request({
+    "method": "GET",
+    "uri": url,
+    "json": true,
+    "headers": {
+      "User-Agent": "MicroserviceNodeJS"
+    }
+  }).then(function (body) {
+    //Só prossegue com o cadastro se o idCriador for equivalente a um usuário na API externa
+    next() 
+  }
+    , function (err) {
+      var erroAPIExterna = err.error
+      console.error("Erro da API externa:" + erroAPIExterna.mensagemDesenvolvedor);
+      res.status(erroAPIExterna.status).json({ erro: erroAPIExterna.titulo })
     });
 });
 
