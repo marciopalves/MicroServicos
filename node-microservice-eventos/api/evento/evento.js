2477b3cfb3102ec180efa18bde8f8fc5379095f8
pedrofsn <pedrokra@gmail.com>
2017-10-21 15:30:55 -0200

    Removendo arquivo não utilizado, identando código e alterando os tipos de dados do schema

--- node-microservice-eventos/api/evento/evento.js
+++ node-microservice-eventos/api/evento/evento.js
@@ -6,12 +6,12 @@ const eventoSchema = new mongoose.Schema({
   descricao: { type: String, required: true },
   dataInicio: { type: String, required: true },
   dataFim: { type: String, required: true },
-  tipo: { type: String, required: true },
+  tipo: { type: Number, required: true },
   endereco: { type: String, required: true },
-  quantidadePessoas: { type: String, required: true },
-  quantidadeProfissionais: { type: String, required: true },
-  idCriador: { type: String, required: true },
-  status: { type: String, required: true }
+  quantidadePessoas: { type: Number, required: true },
+  quantidadeProfissionais: { type: Number, required: true },
+  idCriador: { type: Number, required: true },
+  status: { type: Number, required: true }
 }, {timestamp: true})
 
 module.exports = restful.model('Evento', eventoSchema)
--- node-microservice-eventos/api/evento/eventoService.js
+++ node-microservice-eventos/api/evento/eventoService.js
@@ -1,28 +1,29 @@
 var request = require('request-promise');
 const Evento = require('./evento')
+const url_api_externa = "http://localhost:8080/usuarios/"
 
 Evento.methods(['get', 'post', 'put', 'delete'])
-Evento.updateOptions({new : true, runValidators : true}) 
+Evento.updateOptions({ new: true, runValidators: true })
 
-Evento.after('get', function(req, res, next) {
-    var eventos = res.locals.bundle
+Evento.after('get', function (req, res, next) {
+  var eventos = res.locals.bundle
 
-    request({
-        "method":"GET", 
-        "uri": "https://api.github.com/",
-        "json": true,
-        "headers": {
-          "User-Agent": "My little demo app"
-        }
-      }).then(function(body) {
-        console.log("Dados oriundos do microserviço:" + body)
-        res.json({ eventos , criadores:body})
+  request({
+    "method": "GET",
+    "uri": url_api_externa,
+    "json": true,
+    "headers": {
+      "User-Agent": "My little demo app"
     }
-      , function (err) {
-        console.error("Falha ao trazer dados do microserviço:" + err);
-        next()
+  }).then(function (body) {
+    console.log("Dados oriundos do microserviço:" + body)
+    res.json({ eventos, criadores: body })
+  }
+    , function (err) {
+      console.error("Falha ao trazer dados do microserviço:" + err);
+      next()
     });
-  });
+});
 
 
 module.exports = Evento
